c3352e69181ef7962f77380cb77f915958e3efc5
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2018-07-21 14:27:01 +0800

    1.打印不同进制
    以八和十六进制打印

---
+++ 1.打印不同进制/源.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	int a = 10;
+	printf("十进制：%d\n八进制：%#o\n十六进制：%#X", a, a, a);
+
+	//Pause
+	int pause=0;
+	while(pause != 1)
+	{
+		printf("\n1 to exit:");
+		scanf("%d", &pause);
+	}
+
+
+	return 0;
+}
