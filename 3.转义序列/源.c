5e735c1454bd3506c0fe9743879424b64ee2adeb
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2018-07-22 12:13:28 +0800

    3.转义序列
    windows下其他的转义序列似乎都没法输出

---
+++ 3.转义序列/源.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	int count;
+
+	printf("警报:\\a\n");
+	printf("\a");
+
+	printf("退格:test\b\n");/*无效？*/
+
+	printf("问号:? \?");/*没啥区别？*/
+
+
+
+	int pause=0;
+	while (pause != 1)
+	{
+		printf("\n1 to exit:\a");
+		scanf("%d", &pause);
+	}
+	return 0;
+}
