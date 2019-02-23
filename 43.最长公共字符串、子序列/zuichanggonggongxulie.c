d01cdd70d22178b6291eec67d5fe6b0caf6da51c
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2019-02-24 03:16:52 +0800

    43.最长公共字符串、子序列

---
+++ 43.最长公共字符串、子序列/zuichanggonggongxulie.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#define LEN 100
+int MCS(char*,char*);
+int max(int *,int);
+int main(void)
+{
+	char str1[LEN],str2[LEN];
+	int num;
+
+	printf("STR1: ");
+	scanf("%s",str1);
+	printf("STR2: ");
+	scanf("%s",str2);
+	
+	num=MCS(str1,str2);
+
+	printf("%d",num);
+
+	
+	
+	return 0;
+}
+
+int MCS(char str1[],char str2[])
+{
+	int graph[strlen(str1)][strlen(str2)];
+	int len1=strlen(str1);
+	int len2=strlen(str2);
+	for(int i=0;i<len1;i++)
+	{
+		for(int ii=0;ii<len2;ii++)
+		{
+			if(str1[i]==str2[ii])
+			{
+				if(i==0||ii==0)
+					graph[i][ii]=1;
+				else
+					graph[i][ii]=graph[i-1][ii-1]+1;
+			}
+			else
+			{
+				if(i==0&&ii==0)
+					graph[i][ii]=0;
+				else if(i==0)
+					graph[i][ii]=graph[i][ii-1];
+				else if(ii==0)
+					graph[i][ii]=graph[i-1][ii];
+				else
+					graph[i][ii]=graph[i-1][ii]>graph[i][ii-1]?graph[i-1][ii]:graph[i][ii-1];
+			}
+		}
+	}
+
+	printf("graph:\n");
+	printf(" %s\n",str2);
+	for(int i=0;i<len1;i++)
+	{
+		printf("%c",str1[i]);
+		for(int ii=0;ii<len2;ii++)
+		{
+			printf("%d",graph[i][ii]);
+		}
+		putchar('\n');
+	}
+
+	return max((int*)graph,len1*len2);
+}
+
+int max(int in[],int len)
+{
+	int temp=in[0];
+	for(int i=1;i<len;i++)
+		if(in[i]>temp)
+			temp=in[i];
+	return temp;
+}
+
